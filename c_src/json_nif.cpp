e04ec53ebbefe9a68cc4e4d545517f21cda15fd6
patrick cieplak <patrick@finixpayments.com>
2017-10-03 23:26:26 -0700

    cleaner code

--- c_src/json_nif.cpp
+++ c_src/json_nif.cpp
@@ -1,27 +1,25 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 #include "erl_nif.h"
-
 #include "json.hpp"
 
 using std::string;
+using std::vector;
 
 static ERL_NIF_TERM
 to_csv_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
   ErlNifBinary input, output;
-
   if (argc == 1) {
     if (enif_inspect_iolist_as_binary(env, argv[0], &input)) {
       if (input.size > 0) {
-        std::vector<unsigned char> input_vector;
+        vector<unsigned char> input_vector;
         for (int i = 0; i < input.size; i++)
           input_vector.push_back(input.data[i]);
-        std::string raw_json(input_vector.begin(), input_vector.end());
+        string raw_json(input_vector.begin(), input_vector.end());
         auto parsed_json = json::parse(raw_json);
-        auto csv = json::csv(parsed_json);
+        auto csv         = json::csv(parsed_json);
         enif_alloc_binary(csv.length(), &output);
         const char* out = csv.c_str();
         for (int i = 0; i < csv.length(); i++)
